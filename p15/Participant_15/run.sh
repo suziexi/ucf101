8c10f0d7c7b6e4ef89a939f28af661320007ea90
suziexi <35875758+suziexi@users.noreply.github.com>
2022-12-09 18:58:38 -0500

    Add files via upload

---
+++ p15/Participant_15/run.sh
@@ -0,0 +1,12 @@
+ffmpeg -i dance1.mp4 -vf fps=1/0.2 1/img%03d.png
+ffmpeg -i dance2.mp4 -vf fps=1/0.2 2/img%03d.png
+ffmpeg -i dance3.mp4 -vf fps=1/0.2 3/img%03d.png
+ffmpeg -i dance4.mp4 -vf fps=1/0.2 4/img%03d.png
+ffmpeg -i dance5.mp4 -vf fps=1/0.2 5/img%03d.png
+ffmpeg -i dance6.mp4 -vf fps=1/0.2 6/img%03d.png
+ffmpeg -i dance7.mp4 -vf fps=1/0.2 7/img%03d.png
+ffmpeg -i dance8.mp4 -vf fps=1/0.2 8/img%03d.png
+ffmpeg -i dance9.mp4 -vf fps=1/0.2 9/img%03d.png
+ffmpeg -i dance10.mp4 -vf fps=1/0.2 10/img%03d.png
+ffmpeg -i dance11.mp4 -vf fps=1/0.2 11/img%03d.png
+ffmpeg -i dance12.mp4 -vf fps=1/0.2 12/img%03d.png
